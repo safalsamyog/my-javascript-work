bc00bc2a723b8784b06ca0d3f84cf725d07518af
safal <74084097+safalsamyog@users.noreply.github.com>
2021-01-29 16:17:53 +0545

    Add files via upload

---
+++ learn_javascript/variable.js
@@ -0,0 +1,68 @@
+console.log("hello man how are you");
+let y="safal is hero";
+console.log(y);
+const s=23;
+const v=23;
+const sum=s+v;
+console.log(sum)
+// there are types of variable in javascript 
+
+//number
+let a=123344;
+let c=12333444;
+console.log(a,c);//for printing
+
+//string
+
+let ss="safal is hero";
+let ff="and good boy";
+
+const d=ss+ff;
+console.log(d);
+
+// boolean variables
+
+var k=true;
+var u=false;
+document.write(k,u)
+
+//practice
+
+//simple calculator
+
+const pp=12;
+const kk=13;
+
+const summ=pp+kk;
+const sub=pp-kk;
+const mul=pp*kk;
+const div=pp/kk;
+const mod=pp%kk;
+
+console.log(summ,sub,mul,div,mod);
+
+//operators with true and false 
+let f=12;
+let g=13;
+
+console.log(f<g,f>g,f==g);
+
+//string technique
+let ww="safal is don";
+ww+=" no he is hero";
+console.log(ww)
+
+
+let cc=2;
+cc++;
+console.log(cc)
+let rr=4;
+rr--;
+console.log(rr)
+let yy=2;
+yy**=2;
+console.log(yy)
+
+
+
+
