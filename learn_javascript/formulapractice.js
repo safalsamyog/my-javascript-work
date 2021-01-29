bcb3704aa8040f21f193eb03e36cbf412e232c2b
safal <74084097+safalsamyog@users.noreply.github.com>
2021-01-29 16:32:44 +0545

    Add files via upload

---
+++ learn_javascript/formulapractice.js
@@ -0,0 +1,40 @@
+//formula programs....
+const radius=12;
+const pi=22/7;
+
+circumference_circle=2*pi*radius;
+console.log(circumference_circle)
+
+//simple interest
+let p=12000;
+let t=2;
+let r=10;
+const si=p*t*r/100;
+console.log(si)
+
+//usd to npr
+
+let usd=12;
+const npr=usd*113;
+console.log(npr);
+
+//npr to usd
+let nr=123;
+const amrican=nr/113;
+console.log(amrican);
+
+//decimal value
+
+let dec=12.15;
+let rt=12.3;
+const df=dec*rt;
+
+console.log(df);
+
+// area of traingle
+
+const length=12;
+const breadth=23;
+
+const area_t=1/2*length*breadth;
+console.log(area_t)
