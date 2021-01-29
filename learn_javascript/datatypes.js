37b2c0ba005637330364fc67619ec0f564997852
safal <74084097+safalsamyog@users.noreply.github.com>
2021-01-29 16:40:20 +0545

    Add files via upload

---
+++ learn_javascript/datatypes.js
@@ -0,0 +1,23 @@
+const a="safal";
+const b=12;
+let s=a+b;
+console.log(s);
+
+var t;
+t="helllo js programmer";
+console.log(t);
+
+
+// string
+const v="hello";
+console.log(v);
+
+//numeric
+const r=123;
+console.log(r);
+
+//boolean
+const g=true;
+const f=false;
+
+console.log(g,f);
